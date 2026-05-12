4cfbfc11b69591a864da18afa8a965081058c24b
Nicolas Gomez <nicogomez@frba.utn.edu.ar>
2026-05-12 17:07:40 -0300

    Primer Commit

---
+++ main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+
+    printf("%s \n","Hola Sintaxis y Semantica de los Lenguahes 2026");
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Sintaxis y Semantica de Los Lenguajes
+Este es mi repositorio para la cursada de Sintaxis y Semántica de Lenguajes 2026
